0ca65c9a380bcf25b956add8ab11d860e842fa3d
Pandolf <pandolf99@gmail.com>
2024-09-16 17:21:37 -0400

    minimal server

--- main.go
+++ main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"fmt"
-	_ "net/http"
+	"net/http"
+	"os"
 )
 
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+func run() error {
+	m := http.NewServeMux()
+	m.HandleFunc("GET /{$}", index)
+	srv := &http.Server{
+		Addr: "localhost:5000",
+		Handler: m,
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello World")
+	err := run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s", err)
+		os.Exit(1)
+	}
 }
